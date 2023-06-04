c52c268e7aa2078bf919c00f228a9569c737d84c
truong <truongphatroy@gmail.com>
2023-06-05 03:42:08 +0900

    fixed feedback_list.php

--- feedback_list.php
+++ feedback_list.php
@@ -1,23 +1,28 @@
 <?php
-require "./components/header.php";
-echo "<h1>list feedback here</h1>";
-$sql = "SELECT name, email, body * FROM feedback";
-
-if ($connection != null) {
-    try {
-        $statement = $connection->prepare($sql);
-        $statement->execute();
-        $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
-        $feedbacks = $statement->fetchAll();
-        foreach ($feedbacks as $feedback) {
-            $name = $feedback["name"] ?? "";
-            $email = $feedback["email"] ?? "";
-            $body = $feedback["body"] ?? "";
-            echo "$name, $email, $body <br />";
+    require 'components/header.php'; 
+    echo "<h1>List of feedbacks here</h1>";
+    $sql = "SELECT name, email, body from feedback;";
+    if($connection != null) {
+        try {
+            $statement = $connection->prepare($sql);
+            $statement->execute();
+            $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+            $feedbacks = $statement->fetchAll();
+            echo '<ul class="list-group">';
+            foreach($feedbacks as $feedback) {
+                $name = $feedback['name'] ?? '';
+                $email = $feedback['email'] ?? '';
+                $body = $feedback['body'] ?? '';
+                echo "<li class='list-group-item'>";
+                echo "<p>$name</p>";
+                echo "<p>$email</p>";
+                echo "<p>$body</p>";
+                echo "</li>";
+                //echo "<h3>$name, $email, $body</h3>";
+            }
+            echo '</ul>';
+        }catch (PDOException $e) {
+            // echo "Cannot query data. Error: " . $e->getMessage;
         }
-        // echo $statement;
-        // echo "links";
-    } catch (PDOException $e) {
-    };
-};
-include "./components/footer.php";
+    }
+    include 'components/footer.php';
